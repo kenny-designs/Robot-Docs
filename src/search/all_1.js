fed1aeaf2bc3f1f23727c1a662130bb01ea3e360
kenny-designs <alexanderman2004@yahoo.com>
2019-09-26 13:32:22 -0400

    first commit

---
+++ src/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['how_20to_20use',['How To Use',['../md_README.html',1,'']]]
+];
---
+++ src/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['getfinalticksandvelocity',['getFinalTicksAndVelocity',['../classRobot.html#a2ff342657f459c4ab2434706d5e9a2d6',1,'Robot']]]
+];
